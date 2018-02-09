7ac350bfbb35cb037bf4b1d6c97ec80e3d2b54be
Benjamin Bouachour <benjamin.bouachour@bioserenity.com>
2018-02-09 10:08:40 +0100

    add pipe, merge map, emptysignal transform Observer array to map, add first way of disposer

--- signals.go
+++ signals.go
@@ -23,11 +23,87 @@ type Observer interface {
 	// ListenCompleted(completed CompletedEvent)
 }
 
+var (
+	idDisp uint64 = 0
+	idSig  uint64 = 0
+)
+
 type Signal struct {
 	// isCompleted bool
-	id        int
+	id        uint64
 	injector  *ChanInjector
-	Observers []Observer
+	Observers map[uint64]Observer
+}
+
+type DisposerList []Disposer
+
+func (dl DisposerList) Dispose() {
+	for _, d := range dl {
+		d.Dispose()
+	}
+}
+
+type Disposer struct {
+	id     uint64
+	signal *Signal
+}
+
+func (d Disposer) Dispose() {
+	if d.signal != nil {
+		delete(d.signal.Observers, d.id)
+	}
+}
+
+func makeDisposer(sig *Signal) Disposer {
+	idDisp++
+	return Disposer{id: idDisp, signal: sig}
+}
+
+func (s *Signal) OnNext(side func(value interface{})) *Signal {
+	// return NewSignal(func (obj Injector){
+	// 	s.Sub
+	// })
+	return nil
+}
+
+func (s *Signal) Map(transformer func(value interface{}) (interface{}, error)) *Signal {
+	pipe := NewPipe()
+	s.ListenNext(func(next interface{}) {
+		newValue, err := transformer(next)
+		if err != nil {
+			pipe.Injector.SendFailed(err)
+		}
+		pipe.Injector.SendNext(newValue)
+
+	})
+	s.ListenFailed(func(err error) {
+		pipe.Injector.SendFailed(err)
+	})
+	s.ListenCompleted(func(completed bool) {
+		pipe.Injector.SendCompleted()
+	})
+	return pipe.Signal
+}
+
+func (s *Signal) Merge(inners ...*Signal) *Signal {
+	pipe := NewPipe()
+	for _, inner := range inners {
+		inner.Listen(func(event interface{}) {
+			pipe.Injector.SendNext(event)
+		}, func(err error) {
+			pipe.Injector.SendFailed(err)
+		}, func(completed bool) {
+			pipe.Injector.SendCompleted()
+		})
+	}
+	s.Listen(func(event interface{}) {
+		pipe.Injector.SendNext(event)
+	}, func(err error) {
+		pipe.Injector.SendFailed(err)
+	}, func(completed bool) {
+		pipe.Injector.SendCompleted()
+	})
+	return pipe.Signal
 }
 
 func (s *Signal) run() {
@@ -39,79 +115,105 @@ L:
 				log.Println("BREAK---failed", s.id)
 				break L
 			}
-			s.SendFailed(err)
+			s.sendFailed(err)
 		case next, ok := <-s.injector.next:
 			if ok == false {
 				log.Println("BREAK---next", s.id)
 				break L
 			}
-			s.SendNext(next)
+			s.sendNext(next)
 		case _, ok := <-s.injector.completed:
 			if ok == false {
 				log.Println("BREAK---completed", s.id)
 				break L
 			}
-			s.SendCompleted()
+			s.sendCompleted()
 			break L
 		}
 	}
 	log.Println("*******END OF RUN*********", s.id)
 }
 
-func (s *Signal) SendNext(v interface{}) {
+func (s *Signal) sendNext(v interface{}) {
 
 	for _, obs := range s.Observers {
 		obs.SendNext(v)
 	}
 }
 
-func (s *Signal) SendCompleted() {
+func (s *Signal) sendCompleted() {
 	for _, obs := range s.Observers {
 		obs.SendCompleted()
 	}
-	s.Observers = []Observer{}
+	s.Observers = map[uint64]Observer{}
 	s.injector.Close()
 }
 
-func (s *Signal) SendFailed(err error) {
+func (s *Signal) sendFailed(err error) {
 	for _, obs := range s.Observers {
 		obs.SendFailed(err)
 	}
 }
 
-func (s *Signal) ListenNext(next NextEvent) {
+func (s *Signal) ListenNext(next NextEvent) Disposer {
 	// log.Println("add listen next :", next)
 	cObs := Observe{next: next}
-	s.Observers = append(s.Observers, cObs)
+	disp := makeDisposer(s)
+	s.Observers[disp.id] = cObs
+	return disp
 }
-func (s *Signal) ListenFailed(failed FailedEvent) {
+func (s *Signal) ListenFailed(failed FailedEvent) Disposer {
 	cObs := Observe{failed: failed}
-	s.Observers = append(s.Observers, cObs)
+	disp := makeDisposer(s)
+	s.Observers[disp.id] = cObs
+	return disp
+
 }
-func (s *Signal) ListenCompleted(completed CompletedEvent) {
+func (s *Signal) ListenCompleted(completed CompletedEvent) Disposer {
 	cObs := Observe{completed: completed}
-	s.Observers = append(s.Observers, cObs)
+	disp := makeDisposer(s)
+	s.Observers[disp.id] = cObs
+	return disp
+
 }
 
-var id = 0
+func (s *Signal) Listen(next NextEvent, failed FailedEvent, completed CompletedEvent) Disposer {
+	cObs := Observe{next: next, failed: failed, completed: completed}
+	disp := makeDisposer(s)
+	s.Observers[disp.id] = cObs
+	return disp
+
+}
+
+func NewEmptySignal() *Signal {
+	i := NewChanInjector()
+	o := make(map[uint64]Observer, 0)
+	s := &Signal{injector: i, Observers: o}
+	s.id = idSig
+	idSig++
+	go s.run()
+	return s
+}
 
 func NewSignal(generator func(obs Injector)) *Signal {
-	i := NewChanInjector()
-	o := make([]Observer, 0, 0)
-	s := &Signal{injector: i, Observers: o}
-	s.id = id
-	id++
-	go s.run()
+	s := NewEmptySignal()
 	go generator(s.injector)
 	return s
 }
 
-func NewPipe() (*Signal, Injector) {
+type Pipe struct {
+	Signal   *Signal
+	Injector Injector
+}
+
+func NewPipe() Pipe {
 	i := NewChanInjector()
-	o := make([]Observer, 0, 0)
+	o := make(map[uint64]Observer, 0)
 	s := &Signal{injector: i, Observers: o}
+	s.id = idSig
+	idSig++
 	go s.run()
-	return s, i
+	return Pipe{Signal: s, Injector: i}
 }
 
 type ChanInjector struct {
